58b161b2851de87eef73d64ba6ffb775c3a095bd
ChrisWen960216 <chriswen96@163.com>
2018-02-08 16:12:54 +0800

    Add apply middleware

--- src/Store.ts
+++ src/Store.ts
@@ -7,6 +7,10 @@ class Store {
     this.state = state;
     this.listeners = [];
     this.reducer = reducer;
+
+    this.dispatch = this.dispatch.bind(this);
+    this.getState = this.getState.bind(this);
+    this.subscribe = this.subscribe.bind(this);
   }
 
   public dispatch(action: string): void {
@@ -21,6 +25,21 @@ class Store {
   public subscribe(listener: any): void {
     this.listeners.push(listener);
   }
+}
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'ADD': {
+//       return { state, count: state.count + 1};
+//     }
+//   }
+// }
+
+function applyMiddleware(store: Store, ...args: any[]): void {
+  const enArr: any[] = args.map((middleware: any): any => middleware({
+    dispatch: store.dispatch,
+    getState: store.getState
+  }));
 }
 
 export default Store;
